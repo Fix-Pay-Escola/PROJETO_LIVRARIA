a35e64d8bd6d89e55877e2eca7879881f2b26ad9
isa-mpontes <imthepontes@gmail.com>
2023-11-07 14:58:50 -0300

    alteracoes em como aparece o texto

--- _js/acv.js
+++ _js/acv.js
@@ -1,24 +1,38 @@
-const check = document.getElementById("check");
-
+const check = document.getElementById("check")
 check.addEventListener("change", () => {
-  document.body.classList.toggle("dark");
-  const main = document.querySelector("#btn-add");
-  main.classList.toggle("dark");
+  document.body.classList.toggle("dark")
+  const main = document.querySelector("#btn-add")
+  main.classList.toggle("dark")
   const botao = document.querySelector("#head");
-  botao.classList.toggle("dark");
-  const input = document.querySelector("#btn-del");
-  input.classList.toggle("dark");
+  botao.classList.toggle("dark")
+  const input = document.querySelector("#btn-del")
+  input.classList.toggle("dark")
   const table = document.querySelector(".table");
-  table.classList.toggle("dark");
-  const backgBtn = document.querySelector(".cell");
-  backgBtn.classList.toggle("dark");
-});
-function openModal(){
-  const clicked = document.getElementById('janela_modal')
-  clicked.classList.add('open')
-  clicked.addEventListener('click', (e) =>{
-      if(e.target.id == 'close' || e.target.id == 'janela_modal'){
-          clicked.classList.remove('open')
+  table.classList.toggle("dark")
+  const backgBtn = document.querySelector(".cell")
+  backgBtn.classList.toggle("dark")
+})
+
+function expandText() {
+  document.addEventListener('click', function (e) {
+    if (e.target.classList.contains('truncate-text')) {
+      const td = e.target;
+      const tr = td.parentElement;
+
+      if (td.getAttribute('data-expanded') === 'true') {
+        td.style.whiteSpace = 'nowrap'
+        td.style.overflow = 'hidden'
+        td.style.textOverflow = 'ellipsis'
+        td.style.height = '1.2em'
+        tr.style.verticalAlign = 'top'
+        td.setAttribute('data-expanded', 'false')
+      } else {
+        td.style.whiteSpace = 'normal'
+        td.style.overflow = 'visible'
+        td.style.height = 'auto'
+        tr.style.verticalAlign = 'middle'
+        td.setAttribute('data-expanded', 'true')
       }
-  })
+    }
+  });
 }
